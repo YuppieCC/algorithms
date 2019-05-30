9a777f888a36b66f34d7de4d3a31aa0347d8addf
zhang yu sen <eacentcheung@gmail.com>
2019-05-30 21:22:15 +0800

    finish removeNthFromEnd

--- Junior/linkedlist/rnnfe.py
+++ Junior/linkedlist/rnnfe.py
@@ -1,44 +1,33 @@
 # Definition for singly-linked list.
-class ListNode:
-    def __init__(self, x):
-        self.val = x
-        self.next = None
+# class ListNode:
+#     def __init__(self, x):
+#         self.val = x
+#         self.next = None
 
-# not complete
 class Solution:
     def removeNthFromEnd(self, head, n: int):
         if head.next is None:
-            return head
+            # 只有一个值
+            return []
         point = head
         while True:
             temp = point
-            print(temp.val)
             for _ in range(n):
+                # 当前的 temp 在 n 个之后是否是 None
                 _cur = temp.next
                 temp = _cur
+            if temp is None:
+                # 去掉第一个
+                point.val = point.next.val
+                point.next = point.next.next
+                return head
             if temp.next is None:
                 next_node = point.next
                 after_node = point.next.next
+                if after_node is None: # n 为 1 ，去掉最后一个
+                    point.next = None
+                    return head
                 next_node.val = after_node.val
                 next_node.next = after_node.next
-                break
+                return head
             point = point.next
-
-        while True:
-            if head.next is None:
-                print(head.val)
-
-                break
-            print(head.val)
-            head = head.next
-
-
-if __name__ == '__main__':
-    _1, _2, _3, _4, _5 = ListNode(1), ListNode(2), ListNode(3), ListNode(4), ListNode(5)
-    _1.next = _2
-    _2.next = _3
-    _3.next = _4
-    _4.next = _5
-    _5.next = None
-    n = 2
-    s = Solution().removeNthFromEnd(_1, 2)
